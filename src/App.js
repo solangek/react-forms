051f478e34de55b5dca3510bb02da42b2bf469b8
Sol <sodanabe@gmail.com>
2023-03-24 00:12:15 +0200

    added Form2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MyForm from './components/MyForm';
+import MyForm2 from "./components/MyForm2";
 
 function App() {
     return (
         <div className="App container">
-        <MyForm />
+            <h2>First example</h2>
+            <MyForm />
+            <h2>Second example</h2>
+            <MyForm2 />
         </div>
     );
 }
--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,37 +1,66 @@
 import React, { useState } from 'react';
 
 function MyForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
+    // form inputs are stored as separate states
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
     const [result, setResult] = useState('');
 
+    /**
+     * Handle input change in the form. Note that the name of the input
+     * is used to determine which state to update.
+     * @param event
+     */
     function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
+        const name = event.target.name;
+        const value = event.target.value;
+        switch (name) {
+            case "name":
+                setName(value);
+                break;
+            case "email":
+                setEmail(value);
+                break;
+            case "message":
+                setMessage(value);
+                break;
+            default:
+                break;
+        }
     }
 
     function handleSubmit(event) {
         event.preventDefault();
-        setResult(JSON.stringify(formData));
+        setResult(JSON.stringify({ name, email, message })); // for demonstration purposes only
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                <label class="form-label">Name:</label>
-                <input type="text" name="name" className="form-control" value={formData.name} onChange={handleInputChange}/>
-                <label class="form-label">Email: </label>
-                <input type="email" name="email" className="form-control" value={formData.email} onChange={handleInputChange} />
-                <label class="form-label">Message: </label>
-                <textarea name="message" className="form-control"  value={formData.message} onChange={handleInputChange}></textarea>
-                <button class="btn btn-primary m-3" type="submit">Submit</button>
+                <label className="form-label">Name:</label>
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    value={name || ""} // why this || ""? because if the value is undefined, React will complain
+                    onChange={handleInputChange}/>
+                <label className="form-label">Email: </label>
+                <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    value={email || ""}
+                    onChange={handleInputChange} />
+                <label className="form-label">Message: </label>
+                <textarea
+                    name="message"
+                    className="form-control"
+                    value={message || ""}
+                    onChange={handleInputChange}></textarea>
+                <button className="btn btn-primary m-3" type="submit">Submit</button>
                 </div>
             </form>
 
